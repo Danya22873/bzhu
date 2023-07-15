ff9edcddf8606d6001d5c909e63593d0a70dc9df
Danya22873 <66486279+Danya22873@users.noreply.github.com>
2023-07-15 13:02:42 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,114 @@
-let calories = 2300,
-    carbs = 275,
-    fats = 80,
-    proteins = 130;
+document.addEventListener("DOMContentLoaded", function() {
+    let sexInput = document.querySelector("#sex");
+    let ageInput = document.querySelector(".age");
+    let heightInput = document.querySelector(".height");
+    let weightInput = document.querySelector(".weight");
+    let activityInput = document.querySelector("#activity");
+    let sex = localStorage.getItem("sex");
+    let age = localStorage.getItem("age");
+    let height = localStorage.getItem("height");
+    let weight = localStorage.getItem("weight");
+    let activity = localStorage.getItem("activity");
+
+    // Set the values in the fields if they exist
+    if (sex) {
+        sexInput.value = sex;
+    }
+    if (age) {
+        ageInput.value = age;
+    }
+    if (height) {
+        heightInput.value = height;
+    }
+    if (weight) {
+        weightInput.value = weight;
+    }
+    if (activity) {
+        activityInput.value = activity;
+    }
+});
+
+
+let calories = 0,
+    carbs = 0,
+    fats = 0,
+    proteins = 0,
+    calculateBtn = document.querySelector(".calc"),
+    caloriesText = document.querySelector(".caloriesStandard"),
+    proteinsText = document.querySelector(".proteinsStandard"),
+    fatsText = document.querySelector(".fatsStandard"),
+    carbsText = document.querySelector(".carbsStandard");
+
+calculateBtn.addEventListener("click",()=>{
+    let sex = document.querySelector("#sex").value,
+        age = document.querySelector(".age").value,
+        height = document.querySelector(".height").value,
+        weight = document.querySelector(".weight").value,
+        activity = document.querySelector("#activity").value;
+
+    if (sex ==="man" && age != 0 && height != 0 && weight != 0 ){
+        calories = Math.ceil((66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)) * activity);
+    }else if(sex ==="woman" && age != 0 && height != 0 && weight != 0 ){
+        calories = Math.ceil((655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age)) * activity);
+    }
+
+    let min = calories - calories * 0.1,
+        max = calories - calories * 0.2;
+
+    carbs = Math.round((((max*0.35)/4) + ((min*0.4)/4)) / 2);
+    fats = Math.round((((max*0.3)/9) + ((min*0.35)/9)) / 2);
+    proteins = Math.round((((max*0.25)/4) + ((min*0.3)/4)) / 2);
+
+    caloriesText.textContent = calories;
+    proteinsText.textContent = proteins;
+    fatsText.textContent = fats;
+    carbsText.textContent = carbs;
+
+    localStorage.setItem("calories", calories);
+    localStorage.setItem("proteins", proteins);
+    localStorage.setItem("fats", fats);
+    localStorage.setItem("carbs", carbs);
+    localStorage.setItem("sex", sex);
+    localStorage.setItem("age", age);
+    localStorage.setItem("height", height);
+    localStorage.setItem("weight", weight);
+    localStorage.setItem("activity", activity);
+
+    proteinPercent.textContent = proteins - proteinsNow.value;
+    localStorage.setItem("proteinsNow", proteinsNow.value);
+
+    fatsPercent.textContent = fats - fatsNow.value;
+    localStorage.setItem("fatsNow", fatsNow.value);
+
+    carbsPercent.textContent = carbs - carbsNow.value;
+    localStorage.setItem("carbsNow", carbsNow.value);
+
+    caloriesPercent.textContent = calories - caloriesNow.value;
+    localStorage.setItem("caloriesNow", caloriesNow.value);
+})
+
+if(localStorage.getItem("proteins")){
+    proteins = localStorage.getItem("proteins");
+    proteinsText.textContent = proteins;
+}
+if(localStorage.getItem("calories")){
+    calories = localStorage.getItem("calories");
+    caloriesText.textContent = calories;
+}
+if(localStorage.getItem("fats")){
+    fats = localStorage.getItem("fats");
+    fatsText.textContent = fats;
+}
+if(localStorage.getItem("carbs")){
+    carbs = localStorage.getItem("carbs");
+    carbsText.textContent = carbs;
+}
 
 let carbsNow = document.querySelector(".carbs"),
     fatsNow = document.querySelector(".fats"),
     proteinsNow = document.querySelector(".protein"),
-    caloriesNow = document.querySelector(".calories");
-
-let proteinPercent = document.querySelector(".proteinPercent"),
+    caloriesNow = document.querySelector(".calories"),
+    proteinPercent = document.querySelector(".proteinPercent"),
     fatsPercent = document.querySelector(".fatsPercent"),
     carbsPercent = document.querySelector(".carbsPercent"),
     caloriesPercent = document.querySelector(".caloriesPercent");
@@ -85,4 +185,33 @@ addBtn.addEventListener("click",()=>{
 
 })
 
+let clearBtn = document.querySelector(".clear-btn");
 
+clearBtn.addEventListener("click",()=>{
+    caloriesNow.value = 0;
+    localStorage.setItem("caloriesNow", caloriesNow.value);
+    caloriesPercent.textContent = calories;
+
+    carbsNow.value = 0;
+    localStorage.setItem("carbsNow", carbsNow.value);
+    carbsPercent.textContent = carbs;
+
+    proteinsNow.value = 0;
+    localStorage.setItem("proteinsNow", proteinsNow.value);
+    proteinPercent.textContent = proteins;
+
+    fatsNow.value= 0;
+    localStorage.setItem("fatsNow", fatsNow.value);
+    fatsPercent.textContent =fats;
+})
+
+
+let openPopup = document.querySelector(".baza"),
+    popup = document.querySelector(".overlay"),
+    closePopup = document.querySelector(".close");
+openPopup.addEventListener("click",()=>{
+    popup.classList.toggle("active")
+})
+closePopup.addEventListener("click",()=>{
+    popup.classList.toggle("active")
+})
